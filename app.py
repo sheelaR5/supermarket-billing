4723e0bca8d183aaaeebb781bd94808d21b51da7
SHEELA R <sheelabalaji599@gmail.com>
2026-04-18 16:05:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+class Customer:
+    def __init__(self, id, name, phone):
+        self.id = id
+        self.name = name
+        self.phone = phone
+
+
+class Product:
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+
+class Bill:
+    def __init__(self, bill_id, customer, product, quantity):
+        self.bill_id = bill_id
+        self.customer = customer
+        self.product = product
+        self.quantity = quantity
+        self.total_amount = product.price * quantity
+
+@app.route("/")
+def home():
+    customer = Customer(1, "Sheela", "9876543210")
+    product = Product(1, "Rice", 50)
+    bill = Bill(1, customer, product, 2)
+
+    return render_template("index.html", bill=bill)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
